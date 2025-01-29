2cf3946d41b7df0bfba8a14e06ab3d31f7752e67
mathewgarfield <abdul.hannan@launchbox.pk>
2025-01-29 20:18:27 +0500

    Pull

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { AuthenticationRoutes } from "./routes/AuthenticationRoutes";
 import { normalRoutes } from "./routes/normalRoutes";
 
 function App() {
+
   return (
     <Routes>
       <Route path="/" exact element={<Splash />} />
+      
       {AuthenticationRoutes.map((route) => {
         return (
           <Route path={route?.url} element={route?.page} key={route?.title} />
